4d276fc5155c4d7d20daa2a6b457ba0c35129a2f
Domenico159 <domenico.zummo@gmail.com>
2021-04-21 14:21:30 +0200

    Add base file and click arrow

---
+++ js/script.js
@@ -0,0 +1,54 @@
+
+const main = new Vue({
+
+    el:'#main',
+    data:{
+        images:[
+            {
+                nome:'Beach',
+                url:'./img/beach-2836300_1280.jpg',
+            },
+            {
+                nome:'Bora Bora',
+                url:'./img/bora-bora-685303_1280.jpg',
+            },
+            {
+                nome:'Maldive',
+                url:'./img/maldives-1993704_1280.jpg',
+            },
+            {
+                nome:'Pholinesia',
+                url:'./img/polynesia-3021072_1280.jpg',
+            },
+            {
+                nome:'Sunrise',
+                url:'./img/sunrise-1014712_1280.jpg',
+            },
+        ],
+
+        indexImg:0,
+
+    },
+    methods:{
+
+        nextImg(){
+
+            this.indexImg++;
+
+            if(this.indexImg > (this.images.length - 1) ){
+                this.indexImg = 0;
+            }
+
+        },
+        prevImg(){
+
+            this.indexImg -= 1;
+
+            if(this.indexImg < 0){
+                this.indexImg = this.images.length - 1;
+            }
+
+        },
+    },
+
+});
